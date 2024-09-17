061b98882ae14fae43ad391d5944378b2ca4a146
Himanshuroy1 <125000148+Himanshuroy1@users.noreply.github.com>
2024-09-17 15:16:15 +0530

    Create module.sol

---
+++ module.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC20Token is ERC20 , Ownable{
+    constructor() ERC20("Major", "GT") Ownable(msg.sender) {
+        _mint(msg.sender, 10 * 100 ** decimals()); 
+    }
+
+    function mint(address recipient, uint256 amount) external onlyOwner {
+        _mint(recipient, amount);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function transfer(address from, address to, uint256 amount) public returns (bool) {
+        require(balanceOf(msg.sender) >= amount, "Insufficient balance");
+        _transfer(from, to, amount);
+        return true;
+    }
+}
